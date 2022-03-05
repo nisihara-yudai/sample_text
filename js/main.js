18e9271671e6cc85551193ce8d2f97452b2a82b9
nishi18masa@gmail.com <masa18nishi@gmail.com>
2022-03-06 08:16:50 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,128 @@
+'use strict';
+
+// document.write("JavaScriptを学習中です");
+// 画面に文章を表示させよう
+
+// alert("ご注意下さい");
+// 警告ウィンドウで注意を引こう
+// const button = document.getElementsByClassName("button");
+// function button=function() {
+//   document.write(60 * 60 * 24 * 365);
+// }
+
+// 一年が何秒か計算しよう
+
+// function text(number1, number2) {
+//   const value = number1 + number2;
+//   return value;
+// }
+
+// document.write(text(81, 9));
+
+// const array = [];
+// 空の配列を作る
+
+// 20点以下だったらだめ、50点以上だったら普通、80点以上だったらすごい、この条件を満たすif文を作る
+// let score = 0;
+// if (score >= 80) {
+//   document.write("すごい");
+// } else if (score >= 50) {
+//   document.write("普通");
+// }else if (score >= 20){
+//   document.write("駄目！");
+// } else {
+//   document.write("最悪！");
+// }
+
+// score = function () => {
+//   let score = 80;
+//   if (score >= 80) {
+//     return "すごい";
+//   } else if (score >= 50){
+//     return "普通";
+//   }
+// }
+
+
+// if (score >= 80) {
+//   document.write("すごい");
+// } else if (score >= 50) {
+//   document.write("普通");
+// }else if (score >= 20){
+//   document.write("駄目！");
+// } else {
+//   document.write("最悪！");
+// }
+
+// 配列を記載する
+// const array = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+// もしも、配列が偶数ならば、コンソールに出力して下さい。もしも奇数ならば、数値を出力して下さい。
+// for (let i = 0; i <= array.length; i++){
+//   if (i % 2 == 0) {
+//     console.log("偶数です");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// 定数numは1~50までの出力をします。もしも3で割り切れるのであれば、「yes」とブラウザに表示し、5で割り切れるのであれば「no」、両方で割り切れるのであれば、「yes/no」と表示する。
+// for (let num = 1; num <= 50; num++){
+//   if (num % 15 == 0) {
+//     document.write("yes/no");
+//   } else if (num % 3 == 0) {
+//     document.write("yes");
+//   } else if (num % 5 == 0) {
+//     document.write("no");
+//   } else {
+//     document.write(num);
+//   }
+// }
+
+// これを関数に置き換えるとこうなる
+
+// let number = function (num) {
+//   for (let number = 1; number <= 50; number++){
+//     if (number % 15 == 0) {
+//       console.log("yes/no");
+//     } else if (number % 3 == 0) {
+//       console.log("yes");
+//     } else if (number % 5 == 0) {
+//       console.log("no");
+//     } else {
+//       console.log(number);
+//     }
+//   }
+// }
+
+// number();
+// これがなかったので、処理がされなかった。
+
+
+// for (let num = 1; num <= 50; num++){
+//   if (num % 15 == 0) {
+//     document.write("yes/no");
+//   } else if (num % 3 == 0) {
+//     document.write("yes");
+//   } else if (num % 5 == 0) {
+//     document.write("no");
+//   } else {
+//     document.write(num);
+//   }
+// }
+
+// 猫の種類の配列
+// const cat = ['スコティッシュホールド', 'アメリカンショートヘア', "ラグドール", "ブリティッシュショートヘア", "ロシアンブルー", "マンチカン", "ベンガル猫", "シャム猫", "シンガプーラ", "オシキャット"];
+
+// 繰り返しを関数で記述
+// let cats = function (cat) {
+//   for (let i = 0; i < cat.length; i++){
+//     console.log(i);
+//   }
+// }
+
+// cats(cat);
+
+
+
+
